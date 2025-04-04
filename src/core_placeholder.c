11c0f13af2da2406aeeadb5da6f70c7c15e9650e
ochsec <chris.ochsenreither@tsgs.com>
2025-04-03 19:46:39 -0700

    build workflow for main
    updated build workflow

---
+++ src/core_placeholder.c
@@ -0,0 +1,35 @@
+/**
+ * Copyright Christopher Ochsenreither. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+
+#include <stddef.h>
+
+/**
+ * This is a placeholder file for the aws-sdk-c-core library.
+ * It provides minimal functionality to ensure the build process works.
+ */
+
+/**
+ * Initialize the aws-sdk-c-core library.
+ * This function should be called before using any functionality in the library.
+ */
+int aws_sdk_c_core_init(void) {
+    /* Placeholder for initialization code */
+    return 0; /* Success */
+}
+
+/**
+ * Clean up the aws-sdk-c-core library.
+ * This function should be called when the application is done using the library.
+ */
+void aws_sdk_c_core_clean_up(void) {
+    /* Placeholder for cleanup code */
+}
+
+/**
+ * Get the version of the aws-sdk-c-core library.
+ */
+const char* aws_sdk_c_core_version(void) {
+    return "0.1.0";
+}
